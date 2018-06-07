4cc21213a03eb53bded544e3f8b7f100b00e438f
assisjrs <assisjrs@gmail.com>
2018-06-07 15:22:33 -0300

    inclusão do projeto para upload e download de documentos

---
+++ backend/documentos/src/test/java/com/assisjrs/cloud/documentos/DocumentosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.assisjrs.cloud.documentos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DocumentosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
